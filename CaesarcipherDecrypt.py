ad340a52945284be6cfca58ecd3e81b09e374799
Nathan <nathangl@sbcglobal.net>
2018-04-23 17:02:33 -0400

    Added caesar example encryption

--- CaesarcipherDecrypt.py
+++ CaesarcipherDecrypt.py
@@ -7,7 +7,7 @@ alphabet = string.ascii_uppercase
 
 Maximum_KeySize = len(alphabet)
 
-def getTranslatedMessage(message, key):
+def getDecryptedMessage(message, key):
     translated = ''
     #for each char letter in string message
     for letter in message:
--- client.py
+++ client.py
@@ -30,36 +30,19 @@ class ClientListener(threading.Thread):
     def GUI(self):
         root.title("Input Output")
         root.geometry("200x200")
-        '''
-        def run(self):
-            (data, addrListen) = UDPSock.recvfrom(buf)
-            output['text'] = output['text'] + "Client 2: " + temp + "\n"
-        '''
 
         def sendMessage():
             temp = inputBox.get()
             output['text'] = output['text'] + "Client 1: " + temp + "\n"
             inputBox.delete(0, END)
-            temp = getTranslatedMessage(temp, 5)
+            temp = getEncryptedMessage(temp, 5)
             UDPSock.sendto(temp, addrSend)
-
-            '''
-            def forword(sendPort, listenPort):
-                string = ' '
-                while string:
-                    string = UDPSock.rcvfrom(buf)
-                    if string:
-                        listenPort.sendto(string)
-                    else:
-                        sendPort.shutdown(socket.SHUT_RD)
-                        listenPort.shutdown(socket.SHUT_WR)
-            '''
 
         def listen():
             global messageReceived
             global messageData
             if messageReceived == True:
-                messageData = getTranslatedMessage(messageData, 5)
+                messageData = getDecryptedMessage(messageData, 5)
                 output['text'] = output['text'] + "Client 2: " + messageData + "\n"
                 messageReceived = False
             root.after(500, listen)
@@ -69,12 +52,15 @@ class ClientListener(threading.Thread):
 
         Button(root, width=20, text ="send", command = sendMessage).grid(row=1, column=0, columnspan = 3)
 
-        output = Label(root, text = "", width = 20, height = 2, background="light blue")
+        scrollbar = Scrollbar(root)
+        scrollbar.pack(side = RIGHT, fill = Y)
+
+        output = Label(root, text = "", width = 20, height = 20, background="white", fg = 'blue', yscrollcommand = scrollbar.set)
         output.grid(row=2, column=0, columnspan = 3)
+        output.pack(SIDE = LEFT, fill = BOTH)
+        scrollbar.config(command = output.yview)
 
         listen()
-
-        #master.after(0, listen)
 
     def run(self):
         while True:
@@ -83,94 +69,9 @@ class ClientListener(threading.Thread):
             messageReceived = True
             global messageData
             messageData = data
-            #output['text'] = output['text'] + "Client 2: " + data + "\n"
-            #master.after_idle(500, listen)
-
-
-class Gui(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-
-
-    def run(self):
-        master = Tk()
-        master.title("Input Output")
-        master.geometry("200x200")
-        '''
-        def run(self):
-            (data, addrListen) = UDPSock.recvfrom(buf)
-            output['text'] = output['text'] + "Client 2: " + temp + "\n"
-        '''
-
-        def sendMessage():
-            temp = inputBox.get()
-            output['text'] = output['text'] + "Client 1: " + temp + "\n"
-            inputBox.delete(0, END)
-
-            UDPSock.sendto(temp, addrSend)
-            print 'sent'
-
-            '''
-            def forword(sendPort, listenPort):
-                string = ' '
-                while string:
-                    string = UDPSock.rcvfrom(buf)
-                    if string:
-                        listenPort.sendto(string)
-                    else:
-                        sendPort.shutdown(socket.SHUT_RD)
-                        listenPort.shutdown(socket.SHUT_WR)
-            '''
-
-        inputBox = Entry(master, bg="lightblue")
-        inputBox.grid(row=0, column=0, columnspan = 3)
-
-        Button(master, width=20, text ="send", command = sendMessage).grid(row=1, column=0, columnspan = 3)
-
-        output = Label(master, text = "", width = 20, height = 2, background="light blue")
-        output.grid(row=2, column=0, columnspan = 3)
-
-        #master.after(0, listen)
-
-        master.mainloop()
 
 root = Tk()
-'''
-GuiThread = Gui(root)
-GuiThread.start()
-'''
 clientListenerThread = ClientListener(root)
 clientListenerThread.daemon = True
 clientListenerThread.start()
 root.mainloop()
-
-
-
-
-
-'''
-host = "127.0.0.1"
-if(len(sys.argv) < 3) :
-    print "Enter: python client.py listenport sendport"
-    sys.exit()
-
-listenPort = int(sys.argv[1])
-sendPort = int(sys.argv[2])
-
-buf = 1024
-
-addr = (host,sendPort)
-UDPSock = socket(AF_INET, SOCK_DGRAM)
-while True:
-    #data = input("Enter message or type 'exit'")
-    data = raw_input("Enter message or type 'exit'")
-    #data = data.encode('utf-8')
-    UDPSock.sendto(data, addr)
-    (data, addr) = UDPSock.recvfrom(buf)
-    print("Received message: " + data)
-    if data == "exit":
-        break
-
-UDPSock.close()
-os._exit(0)
-'''
